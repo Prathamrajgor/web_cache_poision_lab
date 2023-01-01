84cdd58e8a3041daeb2bf80a43b9661cac0886c6
Pratham <prathamrajgor@gmail.com>
2023-01-01 18:09:57 +0530

    2

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const app=require("express")()
 const nodeCache=require("node-cache")
-const cache=new nodeCache({stdTTL:15});
+const cache=new nodeCache({stdTTL:10});
 
 
 app.get("/",(req,res)=>{
-    if(cache.has("landing")){
-        console.log(`Existing cache found this reuest:  ${cache.get("landing")}`);
-        res.send(`Thank you for accessing ${cache.get("landing")}`)
+    if(cache.has("poison")){
+        console.log(`Existing cache found for this reuest:  ${cache.get("poison")}`);
+        if((cache.get("poison")).includes("alert(1)")){
+            res.send(`<h3>Congrulations! You have successfully solved the lab</h3><div> Thank you for accessing ${cache.get("poison")} Unfortunately, the site is currently under maintainance</div> `)
+            cache.del("poison")
+        }else{
+            res.send(`Thank you for accessing ${cache.get("poison")}. Unfortunately, the site is currently under maintainance`)
+        }
     }else{
-        cache.set("landing",req.headers.host)
+        cache.set("poison",req.headers.host)
         console.log("Cache set to: ",req.headers.host);
-        res.send(`Thank you for accessing ${encodeURIComponent(req.headers.host)}`)
+        res.send(`Thank you for accessing ${encodeURIComponent(req.headers.host)} Unfortunately, the site is currently under maintainance`)
     }
 });
 app.listen(5000,(err)=>{
